f2c69cfa59172ff24dcaebec5a60426378504cf4
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2019-12-20 01:11:22 +0200

    Add color palette to the theme

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,66 @@
-export const theme = {};
+export const theme = {
+  /*
+    Primary actions, links, navigation items, icons,
+    accent borders, text to emphasize
+  */
+  blue1: "#003E6B",
+  blue2: "#0A558C",
+  blue3: "#0F609B",
+  blue4: "#186FAF",
+  blue5: "#2680C2",
+  blue6: "#4098D7",
+  blue7: "#62B0E8",
+  blue8: "#84C5F4",
+  blue9: "#B6E0FE",
+  blue10: "#DCEEFB",
+  /*
+    Accent color
+  */
+  yellow1: "#8D2B0B",
+  yellow2: "#B44D12",
+  yellow3: "#CB6E17",
+  yellow4: "#DE911D",
+  yellow5: "#F0B429",
+  yellow6: "#F7C948",
+  yellow7: "#FADB5F",
+  yellow8: "#FCE588",
+  yellow9: "#FFF3C4",
+  yellow10: "#FFFBEA",
+  /*
+    Most of text, backgrounds, borders,
+    secondary buttons and links
+  */
+  bluegrey1: "#102A43",
+  bluegrey2: "#243B53",
+  bluegrey3: "#334E68",
+  bluegrey4: "#486581",
+  bluegrey5: "#627D98",
+  bluegrey6: "#829AB1",
+  bluegrey7: "#9FB3C8",
+  bluegrey8: "#BCCCDC",
+  bluegrey9: "#D9E2EC",
+  bluegrey10: "#F0F4F8",
+  /*
+    Support colors
+  */
+  cyan1: "#044E54",
+  cyan2: "#0A6C74",
+  cyan3: "#0E7C86",
+  cyan4: "#14919B",
+  cyan5: "#2CB1BC",
+  cyan6: "#38BEC9",
+  cyan7: "#54D1DB",
+  cyan8: "#87EAF2",
+  cyan9: "#BEF8FD",
+  cyan10: "#E0FCFF",
+  red1: "#610404",
+  red2: "#780A0A",
+  red3: "#911111",
+  red4: "#A61B1B",
+  red5: "#BA2525",
+  red6: "#D64545",
+  red7: "#E66A6A",
+  red8: "#F29B9B",
+  red9: "#FACDCD",
+  red10: "#FFEEEE",
+};
