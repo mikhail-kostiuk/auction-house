4c2b9e080aab2a81fe4414e327749a8e074e56da
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2019-12-31 15:28:21 +0200

    Refactror structure and styles of sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import SidebarNavigation from "./sidebarNavigation/SidebarNavigation";
-import { SidebarWrapper } from "./SidebarStyles";
 
 function Sidebar() {
   return (
-    <SidebarWrapper>
-      <SidebarNavigation></SidebarNavigation>
-    </SidebarWrapper>
+    <aside>
+      <SidebarNavigation />
+    </aside>
   );
 }
 
--- src/components/sidebar/SidebarStyles.js
+++ src/components/sidebar/SidebarStyles.js
@@ -1,5 +1,1 @@
-import styled from "styled-components";
 
-export const SidebarWrapper = styled.aside`
-  width: 210px;
-`;
