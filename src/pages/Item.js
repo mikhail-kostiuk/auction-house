b35428293e98849acf934bab008a3cf30527d0bf
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-01-04 14:19:16 +0200

    Add some basic stylying to the single item page

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -5,8 +5,23 @@ import Header from "../components/header/Header";
 import {
   Page,
   BackLink,
+  ArrowIcon,
   PageContent,
   PageContentContainer,
+  Title,
+  DescriptionTitle,
+  DescriptionText,
+  ImageContainer,
+  Image,
+  Left,
+  Right,
+  LotInfo,
+  StartingBid,
+  TimeLeft,
+  Form,
+  BidsInfo,
+  CurrentBid,
+  Bids
 } from "./ItemStyles";
 
 function Item(props) {
@@ -37,38 +52,57 @@ function Item(props) {
     <Page>
       <Header />
       <PageContentContainer>
-        <BackLink to="/home">Back to Search</BackLink>
+        <BackLink to="/">
+          <ArrowIcon>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+            >
+              <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+            </svg>
+          </ArrowIcon>
+          Back to Search
+        </BackLink>
         {item && (
           <PageContent>
-            <h1>{item.title}</h1>
-            <p>{item.description}</p>
-            <div>
-              <img src={item.imageUrl} alt={item.title} />
-            </div>
-            <form>
-              <div>
-                <span>{item.currentBid}</span>
-                <span>{item.bids}</span>
-              </div>
-              <div>
-                <span>{`Starting bid ${item.startingBid}`}</span>
-                <span>{item.endDate.seconds}</span>
-              </div>
-              <div>
-                <span>Quick bid</span>
+            <Left>
+              <Title>{item.title}</Title>
+              <ImageContainer>
+                <Image src={item.imageUrl} alt={item.title} />
+              </ImageContainer>
+              <DescriptionTitle>Description</DescriptionTitle>
+              <DescriptionText>{item.description}</DescriptionText>
+            </Left>
+            <Right>
+              <LotInfo>
+                <StartingBid>{`Starting bid $${item.startingBid}`}</StartingBid>
+                <TimeLeft>{item.endDate.seconds}</TimeLeft>
+              </LotInfo>
+              <Form>
+                <BidsInfo>
+                  <CurrentBid>{item.currentBid}</CurrentBid>
+                  <Bids>{item.bids}</Bids>
+                </BidsInfo>
                 <div>
-                  <button></button>
-                  <button></button>
-                  <button></button>
+                  <span>Quick bid</span>
+                  <div>
+                    <button>{`$${item.currentBid * 1.1}`}</button>
+                    <button>{`$${item.currentBid * 1.5}`}</button>
+                    <button>{`$${item.currentBid * 2}`}</button>
+                  </div>
+                  <div>
+                    <label htmlFor="directBid">Bid directly</label>
+                    <input type="number" name="directBid" id="directBid" />
+                    <button>Place bid</button>
+                  </div>
+                  <button>Add to favorites</button>
                 </div>
-                <div>
-                  <label htmlFor="directBid">Bid directly</label>
-                  <input type="number" name="directBid" id="directBid" />
-                  <button>Place bid</button>
-                </div>
-                <button>Add to favorites</button>
-              </div>
-            </form>
+              </Form>
+            </Right>
           </PageContent>
         )}
       </PageContentContainer>
--- src/pages/ItemStyles.js
+++ src/pages/ItemStyles.js
@@ -1,20 +1,11 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const BackLink = styled(Link)`
-  display: block;
-  color: ${props => props.theme.bluegrey5};
-  text-decoration: none;
-  font-size: 14px;
-  line-height: 18px;
-  border-bottom: 1px solid ${props => props.theme.bluegrey5};
+export const Page = styled.div`
+  /* background-color: ${props => props.theme.bluegrey10}; */
 `;
 
-export const Page = styled.div`
-  background-color: ${props => props.theme.bluegrey10};
-`;
 export const PageContentContainer = styled.div`
-  display: flex;
   width: 100%;
   padding: 10px;
 
@@ -27,6 +18,104 @@ export const PageContentContainer = styled.div`
     margin: 0 auto;
   }
 `;
+
+export const BackLink = styled(Link)`
+  display: inline-flex;
+  width: auto;
+  align-items: center;
+  color: ${props => props.theme.bluegrey5};
+  font-size: 14px;
+  line-height: 18px;
+`;
+
+export const ArrowIcon = styled.div`
+  height: 14px;
+  margin-bottom: -4px;
+  transform: rotate(90deg);
+
+  & svg {
+    height: 100%;
+  }
+`;
+
 export const PageContent = styled.div`
-  /* display: flex; */
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`;
+
+export const Title = styled.h1`
+  flex-shrink: 0;
+  width: 100%;
+  margin: 0;
+  color: ${props => props.theme.bluegrey1};
+  font-size: 32px;
+  font-weight: 400;
+`;
+
+export const DescriptionTitle = styled.span`
+  display: block;
+  padding-top: 20px;
+  margin-top: 20px;
+  border-top: 1px solid ${props => props.theme.bluegrey8};
+  color: ${props => props.theme.bluegrey3};
+  text-transform: uppercase;
+`;
+
+export const DescriptionText = styled.p`
+  color: ${props => props.theme.bluegrey3};
+`;
+
+export const Left = styled.div`
+  width: 48%;
+`;
+
+export const Right = styled.div`
+  width: 48%;
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  margin-top: 20px;
+`;
+
+export const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  object-fit: contain;
+`;
+
+export const LotInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StartingBid = styled.span`
+  color: ${props => props.theme.bluegrey3};
+`;
+
+export const TimeLeft = styled.span`
+  color: red;
+`;
+
+export const Form = styled.form`
+  padding: 20px;
+  margin-top: 20px;
+  background-color: ${props => props.theme.bluegrey10};
+  border: 1px solid ${props => props.theme.bluegrey8};
+`;
+
+export const BidsInfo = styled.div``;
+
+export const CurrentBid = styled.span`
+  font-size: 28px;
+`;
+
+export const Bids = styled.span`
+  display: block;
+  padding: 20px;
+  margin-left: 30px;
+  background-color: #fff;
 `;
