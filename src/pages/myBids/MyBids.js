39a3c8acf4c60073ba746b912335cc354c2ae218
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-01-13 21:05:23 +0200

    Implement my bids page

--- src/pages/myBids/MyBids.js
+++ src/pages/myBids/MyBids.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { firestore } from "../../firebase";
 import { PageContent } from "./MyBidsStyles";
 import PageTemplate from "../pageTemplate/PageTemplate";
-import MyBidsGallery from "../../components/galleries/myBidsGallery/MyBidsGallery";
+import Gallery from "../../components/galleries/gallery/Gallery";
 
-function MyBids() {
+function MyBids(props) {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const { user } = props.auth;
+    const resultSet = [];
+
+    if (user) {
+      firestore
+        .collection("items")
+        .where("lastBidder", "==", user.uid)
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            resultSet.push({ id: doc.id, ...doc.data() });
+          });
+          setItems(resultSet);
+        });
+    } else {
+      props.history.push("/not-found");
+    }
+  }, [props.auth, props.history]);
+
   return (
     <PageTemplate pageTitle="My Bids">
       <PageContent>
-        <MyBidsGallery />
+        <Gallery items={items} maxColumns="4" />
       </PageContent>
     </PageTemplate>
   );
 }
 
-export default MyBids;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth,
+  };
+};
+
+export default connect(mapStateToProps)(MyBids);
