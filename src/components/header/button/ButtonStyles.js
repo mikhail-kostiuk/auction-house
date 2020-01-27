c9c0a0229c07cb285714f8ea46af550a4fd85dd5
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-01-28 01:00:58 +0200

    Add hover effect to header buttons

--- src/components/header/button/ButtonStyles.js
+++ src/components/header/button/ButtonStyles.js
@@ -5,8 +5,12 @@ export const ButtonWrapper = styled.button`
   height: 40px;
   padding: 0 10px;
   border: none;
-  border-radius: 3px;
   color: #fff;
   background-color: ${props => props.theme.blue5};
   font-size: 14px;
+
+  &:hover {
+    background-color: ${props => props.theme.blue4};
+    cursor: pointer;
+  }
 `;
