42e072a67702ff7c3b1431da72027719c3d81cda
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2020-01-27 17:33:22 +0200

    Implement popular categories section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,152 +1,134 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { firestore } from "../../firebase";
-import { buildBasicQuery } from "../../helpers/buildQuery";
+import { setCategories } from "../../actions/categoryActions";
 import PageTemplate from "../pageTemplate/PageTemplate";
 import Slider from "../../components/slider/Slider";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Gallery from "../../components/gallery/Gallery";
-import Pagination from "../../components/pagination/Paginations";
+import bg1 from "../../assets/images/Ancient_Art_&_Antiquitie.jpg";
+import bg2 from "../../assets/images/Classical_Paintings.jpg";
+import bg3 from "../../assets/images/Dutch_Cartography.jpg";
+import bg4 from "../../assets/images/Motobilia.jpg";
+import bg5 from "../../assets/images/Asian_Coins.jpg";
+import bg6 from "../../assets/images/Diamonds.jpg";
+import bg7 from "../../assets/images/Antique_Jewellery.jpg";
+import bg8 from "../../assets/images/Rolex_Watches.jpg";
+
 import {
   PageContent,
-  SidebarContainer,
-  GalleryContainer,
-  PaginationContainer,
+  SliderContainer,
+  SectionHeader,
+  PopularCategoriesContainer,
+  PopularCategory,
 } from "./HomeStyles";
 
 function Home(props) {
-  const [sortOrder, setSortOrder] = useState("TIME_SOONEST");
-  const [totalPages, setTotalPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [resultSet, setResultSet] = useState({
-    items: [],
-    cursor: {
-      next: null,
-      prev: null,
-    },
-  });
-
-  const selectedCategories = props.category;
-  const itemsPerPage = 6;
-
-  useEffect(() => {
-    const query = buildBasicQuery(sortOrder, selectedCategories);
-    const result = [];
-
-    // If category or subcategory is selected, use items array length returned
-    // by query to calculate total pages
-    if (selectedCategories.category || selectedCategories.subcategory) {
-      query.get().then(querySnapshot => {
-        const totalItems = querySnapshot.docs.length;
-        const totalPages = Math.ceil(totalItems / itemsPerPage);
-        setTotalPages(totalPages ? totalPages : 1);
-      });
-    } else {
-      // If category or subcategory is not selected, use internal collection
-      // counter to calculate total pages
-      firestore
-        .collection("lots")
-        .doc("active")
-        .get()
-        .then(doc => {
-          const totalItems = doc.data().numberOfDocs;
-          const totalPages = Math.ceil(totalItems / itemsPerPage);
-          setTotalPages(totalPages ? totalPages : 1);
-        });
-    }
-
-    // Get initial first items
-    query
-      .limit(itemsPerPage)
-      .get()
-      .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-          // doc.data() is never undefined for query doc snapshots
-          result.push({ id: doc.id, ...doc.data() });
-        });
-
-        let next;
-
-        if (result.length < 6) {
-          next = null;
-        } else {
-          const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
-
-          next = query.startAfter(lastVisible).limit(itemsPerPage);
-        }
-
-        setCurrentPage(1);
-        setResultSet({ items: result, cursor: { next } });
-      });
-  }, [selectedCategories, sortOrder]);
-
-  function getItems(cursorQuery) {
-    const result = [];
-    let basicQuery = buildBasicQuery(sortOrder, selectedCategories);
-
-    cursorQuery.get().then(function(documentSnapshots) {
-      documentSnapshots.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        result.push({ id: doc.id, ...doc.data() });
-      });
-
-      let lastVisible =
-        documentSnapshots.docs[documentSnapshots.docs.length - 1];
-      let firstVisible = documentSnapshots.docs[0];
-
-      const next = basicQuery.startAfter(lastVisible).limit(itemsPerPage);
-      const prev = basicQuery.endBefore(firstVisible).limitToLast(itemsPerPage);
-
-      setResultSet({ items: result, cursor: { next, prev } });
-    });
-  }
-
-  function getPrevItems() {
-    getItems(resultSet.cursor.prev);
-    setCurrentPage(currentPage - 1);
-  }
-
-  function getNextItems() {
-    getItems(resultSet.cursor.next);
-    setCurrentPage(currentPage + 1);
-  }
-
-  function getSelectedCategoryName(selectedCategories) {
-    return (
-      selectedCategories.subcategory ||
-      selectedCategories.category ||
-      "All categories"
-    );
-  }
-
-  function handleSortOrderChange(value) {
-    setSortOrder(value);
-    setCurrentPage(1);
-  }
-
   return (
     <PageTemplate>
-      <Slider />
       <PageContent>
-        <SidebarContainer>
-          <Sidebar />
-        </SidebarContainer>
-        <GalleryContainer>
-          <Gallery
-            items={resultSet.items}
-            title={getSelectedCategoryName(selectedCategories)}
-            maxColumns="3"
-            handleSortOrderChange={handleSortOrderChange}
-          />
-          <PaginationContainer>
-            <Pagination
-              totalPages={totalPages}
-              currentPage={currentPage}
-              getNextItems={getNextItems}
-              getPrevItems={getPrevItems}
-            />
-          </PaginationContainer>
-        </GalleryContainer>
+        <SliderContainer>
+          <Slider title="Auctions closing soon" order="endDate" />
+        </SliderContainer>
+        <SectionHeader>Popular Categories</SectionHeader>
+        <PopularCategoriesContainer>
+          <PopularCategory
+            bg={`url(${bg1})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Archaeology",
+                subcategory: "Ancient Art & Antiquitie",
+              });
+            }}
+            to="/explore"
+          >
+            Ancient Art & Antiquitie
+          </PopularCategory>
+          <PopularCategory
+            bg={`url(${bg2})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Art",
+                subcategory: "Classical Paintings",
+              });
+            }}
+            to="/explore"
+          >
+            Classical Paintings
+          </PopularCategory>
+          <PopularCategory
+            bg={`url(${bg3})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Books",
+                subcategory: "Dutch Cartography",
+              });
+            }}
+            to="/explore"
+          >
+            Dutch Cartography
+          </PopularCategory>
+          <PopularCategory
+            bg={`url(${bg4})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Vehicles",
+                subcategory: "Motobilia",
+              });
+            }}
+            to="/explore"
+          >
+            Motobilia
+          </PopularCategory>
+          <PopularCategory
+            bg={`url(${bg5})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Coins",
+                subcategory: "Asian Coins",
+              });
+            }}
+            to="/explore"
+          >
+            Asian Coins
+          </PopularCategory>
+          <PopularCategory
+            bg={`url(${bg6})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Gemstones",
+                subcategory: "Diamonds",
+              });
+            }}
+            to="/explore"
+          >
+            Diamonds
+          </PopularCategory>
+          <PopularCategory
+            bg={`url(${bg7})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Jewellery",
+                subcategory: "Antique Jewellery",
+              });
+            }}
+            to="/explore"
+          >
+            Antique Jewellery
+          </PopularCategory>
+          <PopularCategory
+            bg={`url(${bg8})`}
+            onClick={() => {
+              props.setCategories({
+                category: "Watches",
+                subcategory: "Rolex Watches",
+              });
+            }}
+            to="/explore"
+          >
+            Rolex Watches
+          </PopularCategory>
+        </PopularCategoriesContainer>
+        <SliderContainer>
+          <Slider title="Be first to bid" order="bidsCount" />
+        </SliderContainer>
       </PageContent>
     </PageTemplate>
   );
@@ -156,4 +138,4 @@ const mapStateToProps = state => {
   return { category: state.category };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, { setCategories })(Home);
--- src/pages/home/HomeStyles.js
+++ src/pages/home/HomeStyles.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+// import bg from "../../assets/images/Ancient_Art_&_Antiquitie.jpg";
+// import bg from "../../assets/images/Archaeology.jpg";
 
 export const PageContent = styled.div`
-  display: flex;
-  width: 100%;
+  /* display: flex; */
+  /* width: 100%; */
 `;
 
 export const SidebarContainer = styled.div`
@@ -23,4 +26,50 @@ export const GalleryContainer = styled.div`
 
 export const PaginationContainer = styled.div`
   margin-top: 30px;
+`;
+
+export const SliderContainer = styled.div`
+  width: 100%;
+`;
+
+export const SectionHeader = styled.h2`
+  margin: 20px 0 20px 0;
+  background-color: #fff;
+  color: ${props => props.theme.blue1};
+  font-size: 28px;
+  font-weight: 700;
+  letter-spacing: 0.5px;
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    margin: 30px 0 30px 0;
+  }
+`;
+
+export const PopularCategoriesContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+export const PopularCategory = styled(Link)`
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 180px;
+  width: 49%;
+  background-image: ${props => props.bg};
+  background-size: cover;
+  background-position: center;
+  font-weight: 400;
+  color: #fff;
+  text-decoration: none;
+  letter-spacing: 1px;
+  text-align: center;
+  font-size: 18px;
+
+  @media screen and (min-width: 768px) {
+    width: 24%;
+  }
 `;
